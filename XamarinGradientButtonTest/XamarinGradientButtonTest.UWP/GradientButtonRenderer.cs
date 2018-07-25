219de32897665bb2135badd6f550f6bfa16b5efb
Nico Zhu <v-zhumin@microsoft.com>
2018-07-25 14:52:59 +0800

    Add project files.

---
+++ XamarinGradientButtonTest/XamarinGradientButtonTest.UWP/GradientButtonRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using XamarinGradientButtonTest.UWP;
+using XamarinGradientButtonTest;
+
+[assembly: ExportRenderer(typeof(GradientButton), typeof(GradientButtonRenderer))]
+namespace XamarinGradientButtonTest.UWP
+{
+  
+    public class GradientButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                if (Element.IsSet(Button.BorderColorProperty) && Element.BorderColor != (Color)Button.BorderColorProperty.DefaultValue)
+                {
+                    UpdateBorderColor();
+                }
+
+            }
+        }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == Button.BorderColorProperty.PropertyName)
+            {
+                UpdateBorderColor();
+            }
+        }
+        void UpdateBorderColor()
+        {
+            Control.BorderBrush = Element.BorderColor != Color.Default ? Element.BorderColor.ToGradientBrush() : (Brush)Windows.UI.Xaml.Application.Current.Resources["ButtonBorderThemeBrush"];
+        }
+
+    }
+    internal static class ConvertExtensions
+    {
+        public static Brush ToGradientBrush(this Color color)
+        {
+            var GradientBrush = new LinearGradientBrush();
+            GradientBrush.StartPoint = new Windows.Foundation.Point(0.5, 0);
+            GradientBrush.EndPoint = new Windows.Foundation.Point(0.5, 1);
+            GradientBrush.GradientStops.Add(new GradientStop() { Color = Windows.UI.Colors.LightGray, Offset = 0.0 });
+            GradientBrush.GradientStops.Add(new GradientStop() { Color = color.ToWindowsColor(), Offset = 1.0 });
+            return GradientBrush;
+        }
+
+        public static Windows.UI.Color ToWindowsColor(this Color color)
+        {
+            return Windows.UI.Color.FromArgb((byte)(color.A * 255), (byte)(color.R * 255), (byte)(color.G * 255), (byte)(color.B * 255));
+        }
+
+    }
+}
---
+++ XamarinGradientButtonTest/XamarinGradientButtonTest.iOS/GradientButtonRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinGradientButtonTest;
+using XamarinGradientButtonTest.iOS;
+
+[assembly: ExportRenderer(typeof(GradientButton), typeof(GradientButtonRenderer))]
+
+namespace XamarinGradientButtonTest.iOS
+{
+    public class GradientButtonRenderer : ButtonRenderer
+    {
+        CAGradientLayer gradient;
+        CAShapeLayer shape;
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            gradient = new CAGradientLayer();
+            gradient.Colors = new CGColor[] { UIColor.Gray.CGColor, Element.BorderColor.ToCGColor() };
+
+            shape = new CAShapeLayer();
+            shape.LineWidth = (nfloat)(Element.BorderWidth);
+            shape.StrokeColor = UIColor.Black.CGColor;
+            shape.FillColor = UIColor.Clear.CGColor;
+            gradient.Mask = shape;
+
+            Control.Layer.AddSublayer(gradient);
+            Control.Layer.BorderColor = UIColor.Clear.CGColor;
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+
+            shape.Path = UIBezierPath.FromRect(rect).CGPath;
+            gradient.Frame = rect;
+        }
+    }
+}
